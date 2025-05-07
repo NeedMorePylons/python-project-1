084f03bfcc75ef5dd71233f2c64d92dc4073c429
NeedMorePylons <cjg1617@gmail.com>
2025-05-07 09:58:31 -0500

    first upload

---
+++ User.py
@@ -0,0 +1,138 @@
+import csv
+
+class Usermanager:
+    """
+    Manages user accounts by loading, appending  and overwriting a CSV file.
+    """
+    import User
+
+    def __init__(self):
+        """
+        Initialize the Usermanager with an empty dictionary.
+        """
+        self.accounts = {} # creates a blank dictionary of our users
+
+    def load_users(self):
+        """
+        this function reads CSV file to get user information and save it to a dict
+        and then sets the key = to the first and last name
+        :return: dict of users with names as keys
+        """
+        with open('User Information.csv', 'r', newline='') as file:
+            reader = csv.reader(file)
+            header = next(reader)  # skip first row
+
+            for row in reader:
+                first_name = row[0].strip()
+                last_name = row[1].strip()
+                pin = row[2].strip()
+                amount = row[3].strip() if len(row) > 3 else 500
+
+                user = User(first_name, last_name, pin, amount)
+                new_key =  user.get_name()
+                account_info = {new_key : user}
+                self.accounts.update(account_info)
+        return self.accounts
+
+    def new_user(self, user : User) -> bool:
+        """
+        Adds a new user to the system if they don't already exist.
+
+        :param user: User object to add
+        :return: True if added successfully, False if duplicate
+        """
+        key = user.get_name()
+        if key in self.accounts:
+            return False
+        self.accounts[key] = user
+        self.save_user(user)
+        return True
+
+
+    def save_user(self, user : User) -> None:
+        """
+        This function will append any new users to the CSV File
+
+        :param user: User object to add
+        """
+        with open('User Information.csv', 'a', newline='' ) as file:
+            writer = csv.writer(file)
+            first, last = user.get_name()
+            writer.writerow([first, last, user.get_pin(), str(user.get_amount())])
+
+    def save_all_users(self) -> None:
+        """
+        Rewrites the entire CSV with current account data.
+        Use this to update (deposit/withdraw).
+        """
+        with open('User Information.csv', 'w', newline='') as file:
+            writer = csv.writer(file)
+            writer.writerow(['First Name', 'Last Name', 'PIN', 'Amount'])  # header
+            for user in self.accounts.values():
+                first, last = user.get_name()
+                writer.writerow([first, last, user.get_pin(), str(user.get_amount())])
+
+class User:
+    """
+    This is the information of a single user.
+    """
+    def __init__(self, first_name: str, last_name: str, pin:str, amount:str = '500'):
+        """
+        Initialize a new user object and sets base amount to 500
+        the minimum requirements to open a new account
+
+        :param first_name: User's first name
+        :param last_name: User's last name
+        :param pin: User's PIN code
+        :param amount: Starting balance (as a string, converted to int)
+        """
+
+        self.__first_name = first_name.strip().lower()
+        self.__last_name = last_name.strip().lower()
+        self.__pin = pin.strip()
+        self.__amount = int(amount)
+
+
+    def get_name(self) ->tuple[str,str]:
+        """
+        gets user first name and last name
+
+        :return: Tuple of usernames
+        """
+        return self.__first_name,self.__last_name
+
+    def get_pin(self) -> str:
+        """
+        gets user's pin
+
+        :return: string of pin
+        """
+        return self.__pin
+
+    def get_amount(self) -> int:
+        """
+        gets user's balance
+
+        :return: int of balance
+        """
+        return self.__amount
+
+    def deposit(self, amount: int) -> None :
+        """
+        Adds money to user's balance
+        :param amount:  amount to deposit
+        """
+        if amount > 0:
+            self.__amount += amount
+
+    def withdraw(self, amount: int) -> bool:
+        """
+        subtracts money from the user's balance.
+
+        :param amount: Amount to withdraw
+        :return: True if successful, False if insufficient funds
+        """
+        if 0 < amount <= int(self.__amount):
+            self.__amount -= amount
+            return True
+        return False
---
+++ logic.py
@@ -0,0 +1,113 @@
+from PyQt6.QtWidgets import *
+from gui import *
+from User import  *
+
+
+
+class Logic(QMainWindow, Ui_MainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+
+        self.button_login.clicked.connect(lambda : self.login())
+        self.button_newuser.clicked.connect(lambda : self.add_user())
+        self.button_amount_enter.clicked.connect(lambda : self.enter())
+        self.button_exit.clicked.connect(lambda : self.close_window())
+
+        self.manager = Usermanager()
+        self.manager.load_users()
+
+        self.post_login_elements(False)
+
+
+    def post_login_elements(self, visible):
+        self.radioButton_withdraw.setVisible(visible)
+        self.radioButton_deposit.setVisible(visible)
+        self.label_5.setVisible(visible)
+        self.button_amount_enter.setVisible(visible)
+        self.label_acount_balance.setVisible(visible)
+        self.entrybox_amount.setVisible(visible)
+        self.label_acount_balance_error.setVisible(visible)
+
+
+    def login(self):
+        """
+        This will check that the information is correct when
+        someone tries to log in
+        :return:
+        """
+        first_name = self.entrybox_user_firstname.text().strip().lower()
+        last_name = self.entrybox_user_lastname.text().strip().lower()
+        pin = self.entrybox_user_pin.text().strip().lower()
+
+        key = (first_name, last_name)
+        accounts = self.manager.accounts
+
+        if key in accounts and accounts[key].get_pin() == pin:
+            self.current_user = accounts[key]
+            self.label_acount_balance.setText(f"Balance: ${self.current_user.get_amount()}")
+            self.lable_welcome_message_.setText("Welcome!")
+            self.lable_login_error.setText("")
+            self.post_login_elements(True)
+        else:
+            self.lable_login_error.setText("Invalid login. Please try again.")
+            self.lable_welcome_message_.setText("")
+            self.post_login_elements(False)
+
+
+
+    def add_user(self):
+        first_name = self.entrybox_user_firstname.text().strip().lower()
+        last_name = self.entrybox_user_lastname.text().strip().lower()
+        pin = self.entrybox_user_pin.text().strip().lower()
+
+        if not (first_name and last_name and pin):
+            self.lable_login_error.setText("All fields must be filled.")
+            return
+
+        new_user = User(first_name, last_name, pin)
+        success = self.manager.new_user(new_user)
+
+        if success:
+            self.lable_welcome_message_.setText("User added successfully!")
+            self.lable_login_error.setText("")
+        else:
+            self.lable_login_error.setText("User already exists.")
+
+
+    def enter(self):
+        transaction_amount_text = self.entrybox_amount.toPlainText().strip()
+        if not transaction_amount_text.isdigit():
+            self.label_acount_balance_error.setText("Please enter a valid number")
+            return
+
+        transaction_amount = int(transaction_amount_text)
+        if self.radioButton_withdraw.isChecked():
+            if transaction_amount >self.current_user.get_amount():
+                self.label_acount_balance_error.setText("Insufficient funds")
+
+            success = self.current_user.withdraw(transaction_amount)
+            if success:
+                self.manager.save_all_users()
+                self.label_acount_balance.setText(f"Balance: ${self.current_user.get_amount()}")
+                self.label_acount_balance_error.setText("Withdrawal successful.")
+            else:
+                self.label_acount_balance_error.setText("Insufficient funds")
+
+
+        elif self.radioButton_deposit.isChecked():
+            self.current_user.deposit(transaction_amount)
+            self.manager.save_all_users()
+            self.label_acount_balance.setText(f"Balance: ${self.current_user.get_amount()}")
+            self.label_acount_balance_error.setText("Deposit successful.")
+
+
+        else:
+            self.label_acount_balance_error.setText("Select Deposit or Withdraw.")
+
+
+        self.label_acount_balance.setText(f"Balance: ${self.current_user.get_amount()}")
+
+
+    def close_window(self):
+        self.close()
